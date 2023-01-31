07bb862a5f805f99379431140ac9a3b4fa46b142
Jacob <jacobnetflix33@gmail.com>
2023-01-31 14:57:20 +0800

    First commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ChirpController extends Controller
+{
+    
+    public function index()
+    {
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+   
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
+    }
+
+    
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+   
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+   
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
